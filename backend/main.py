d8c3465e1add8ec6390f1c225994fd63869bee17
Richard Greene <richie@richards-mbp.home>
2024-09-17 10:18:17 +0100

    feat: load json data file and return from test endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+import json
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Load Data
+with open("data.json") as f:
+    music_data = json.load(f)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return music_data
